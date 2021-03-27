81accc0db9be3e38deedfbe8e691e7b6b971a9ca
Tolulope <lanre.toluwa@gmail.com>
2021-03-27 16:25:37 +0100

    notification service completed

--- notifier/notifier.js
+++ notifier/notifier.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const notifier = require("node-notifier");
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require("path");
 
 app.use(bodyParser.json());
+
 app.get("/health", (req, res) => res.status(200).send());
-app.post()
+app.post("/notify", (req, res) => {
+    notify(req.body, callbackFn = (reply) => res.send(reply))
+});
+
 app.listen(port, () => console.log(`Notifier server is now active at port ${port}`));
+
+const notify = ({title, message}, callbackFn) => {
+    notifier.notify(
+        {
+            title: title || "No title",
+            message: message || "No message",
+            icon: path.join(__dirname, "icon.jpg"),
+            sound: true,
+            wait: true,
+            reply: true,
+            appId: "Local Man",
+            closeLabel: "Completed?",
+            timeout: 15,
+
+        },
+        (err, response, reply) => {
+            callbackFn(reply)
+        }
+    );
+};
