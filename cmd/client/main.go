8a78fc9f46272158eda9a55629c93fa19c11392b
Tolulope <lanre.toluwa@gmail.com>
2021-04-30 18:26:41 +0100

    command line's client main file

---
+++ cmd/client/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go-projects/reminder-cli/client"
+	"os"
+)
+
+var (
+	backendURIFlag = flag.String("backend", "http://127.0.0.1:8080", "URI of backend API")
+	helpFlag       = flag.Bool("help", false, "Display help message")
+)
+
+func main() {
+	flag.Parse()
+	s := client.NewSwitch(*backendURIFlag)
+
+	if *helpFlag || len(os.Args) == 1 {
+		s.Help()
+		return
+	}
+
+	err := s.Switch()
+	if err != nil {
+		fmt.Printf("cmd switch error: %s", err)
+		os.Exit(2)
+	}
+}
